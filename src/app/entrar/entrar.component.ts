517970060be1404d3b6878d55236229ea1fafd4f
Iury-Leite <iury.scleite@gmail.com>
2022-06-29 22:18:39 -0300

    2 files modified

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -3,7 +3,7 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.component.html',
-  styleUrls: [ './entrar.component.scss' ]
+ // styleUrls: [ './entrar.component.scss' ]
 })
 export class EntrarComponent  {
  
