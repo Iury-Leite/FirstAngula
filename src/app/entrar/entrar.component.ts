8d8cea3b5030dc27e65b71c68999d09c97c73653
Iury-Leite <iury.scleite@gmail.com>
2022-06-29 22:15:11 -0300

    Initial commit

---
+++ src/app/entrar/entrar.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'app-entrar',
+  templateUrl: './entrar.component.html',
+  styleUrls: [ './entrar.component.scss' ]
+})
+export class EntrarComponent  {
+ 
+}
